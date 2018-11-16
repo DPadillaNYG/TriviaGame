2bbe5165a6ff7e8e796799829a833e5277bd8ac5
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-11-16 11:15:59 -0500

    New Logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,10 +38,18 @@ var trivia = [
 
 ];
 
-var $triviaDiv = $('#trivia-formatting');
-var $loveMusic = $('#love-music');
+// Elemental Variables (for DOM)
+var $triviaDiv = $('#trivia-formatting'); // Main Div
+var $loveMusic = $('#love-music'); // Audio Element
 var $timer = $('<span>');
+var $question = $('<p>');
+var $ul = $('<ul>');
+var $anonymousSig = $('<p>')
+                    .html("<br>Your &nbsp; Secret &nbsp; Admirer")
+                    .attr("id", "sincerely")
+                    .append($('<span>').text("Love,"));
 
+// Standard Game-Specific Variables
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredCount = 0;
@@ -66,7 +74,21 @@ function startTimer() {
     countingDown = setInterval( function() {
         timer--;
         $timer.text(timer);
+        ifTimesUp();
     }, 1000)
+}
+
+function ifTimesUp() {
+    if ( timer === 0 ) {
+        stopTimer();
+        $ul.hide();
+        $question.hide();
+        unansweredCount++;
+        var $pityUser = $("<p>")
+        $pityUser.text('" You ran out of time, Trivia. "');
+        $triviaDiv.append($pityUser);
+        $triviaDiv.append($anonymousSig);
+    }
 }
 
 function stopTimer() {
@@ -82,70 +104,70 @@ function pressStart() {
     $newDiv.append($startButton);
     $('#wrinkled-letter-design').append($newDiv);
 
-    // Creating Start Button Click Event Below
+    // Creating the Start Button Click Event Below
     $startButton.on("click", function() {
         $newDiv.hide();
         autoPlay();
         startTimer();
-        newQuestion(0);
+        createNewTrivia(0);
     });
 }
 
-function newQuestion(movieObj) {
+function createNewTrivia(objIndexNumber) {
     // Creating Question
-    var $question = $("<p>");
     $question.attr("id", "ask-question");
-    $question.text(trivia[movieObj].question);
+    $question.text(trivia[objIndexNumber].question);
     $triviaDiv.append($question);
 
-    // Create Answer Choices
-    var $ul = $("<ul>");
-    for( var i = 0; i <  trivia[movieObj].choices.length; i++) {
-        $ul.append($("<li>").text(trivia[movieObj].choices[i]).attr("choice", trivia[movieObj].choices[i]));
+    // Creating Answer Choices
+    for( var i = 0; i <  trivia[objIndexNumber].choices.length; i++) {
+        $ul.append($("<li>").text(trivia[objIndexNumber].choices[i]).attr("choice", trivia[objIndexNumber].choices[i]));
     }
     $triviaDiv.append($ul);
 
-    // Add/Replace New Image
-    $("main").css("background", "url(" + trivia[movieObj].coverImage + ")");
+    // Creating Cover Image
+    $("main").css("background", "url(" + trivia[objIndexNumber].coverImage + ")");
     $('#wrinkled-letter-design').css("opacity", ".75");
     $("main").css("background-size", "cover");
 
-    // Functionality for Hovering Over Choices 
-    $("li").mouseenter( function() {
-        $(this).css("border", "solid 3px black");
-    }).mouseleave( function() {
-        $(this).css("border", "none");
-    });
-
-    // Creating Letter Signature for "Anonymous"
-    var $anonymousSig = $('<p>').html("<br>Your &nbsp; Secret &nbsp; Admirer").attr("id", "sincerely");
-    var $sincerelySig = $('<span>').text("Love,");
-    $anonymousSig.append($sincerelySig);
+    // Functionality for Hovering Over Choices
+    function $liHoverDesign() {
+        $("li").mouseenter( function() {
+            $(this).css("border", "solid 3px black");
+        }).mouseleave( function() {
+            $(this).css("border", "none");
+        });     
+    } 
 
     // Determining plan of action when user clicks or time runs out
-    $("li").click( function() {
-        if ( $(this).attr("choice") === trivia[movieObj].answer ) {
-            stopTimer();
-            $ul.hide();
-            $question.hide();
-            correctAnswers++;
-            var $praiseUser = $("<p>");
-            $praiseUser.text("That is correct, Trivia <3");
-            $triviaDiv.append($praiseUser);
-            $triviaDiv.append($anonymousSig);
+    function choosingAnAnswer() {
+        $("li").click( function() {
+            if ( $(this).attr("choice") === trivia[objIndexNumber].answer) {
+                stopTimer();
+                $ul.hide();
+                $question.hide();
+                correctAnswers++;
+                var $praiseUser = $("<p>");
+                $praiseUser.text('" That is correct, Trivia <3 "');
+                $triviaDiv.append($praiseUser);
+                $triviaDiv.append($anonymousSig);
+    
+            } else {
+                stopTimer();
+                $ul.hide();
+                $question.hide();
+                incorrectAnswers++;
+                var $curseUser = $("<p>")
+                $curseUser.text('" I\'m sorry Trivia, that is incorrect . "');
+                $triviaDiv.append($curseUser);
+                $triviaDiv.append($anonymousSig);
+            } 
+        });
+    }
 
-        } else {
-            stopTimer();
-            $ul.hide();
-            $question.hide();
-            incorrectAnswers++;
-            var $curseUser = $("<p>")
-            $curseUser.text("I'm sorry Trivia, that is incorrect .");
-            $triviaDiv.append($curseUser);
-            $triviaDiv.append($anonymousSig);
-        }
-    });
-
+    // Inner-Function Calls
+    $liHoverDesign();
+    choosingAnAnswer();
     $triviaDiv.show();
 }
 
